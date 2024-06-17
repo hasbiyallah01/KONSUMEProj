11326a2e954db15626de590debe99e2fbe9ef340
hasbiyallah01 <oyebohm@gmail.com>
2024-06-17 23:29:13 +0100

    prof

--- obj/Debug/net8.0/DaticianProj.AssemblyInfo.cs
+++ obj/Debug/net8.0/DaticianProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DaticianProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83f7a729aacf428b1938fb2262d8a57fa9fb1a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f602e1598beca3d20bde7c46269322f262994794")]
 [assembly: System.Reflection.AssemblyProductAttribute("DaticianProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DaticianProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
